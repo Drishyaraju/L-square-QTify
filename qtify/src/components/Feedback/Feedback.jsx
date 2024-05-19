42010c003c0fa5801ecfe7728cd3547f3a7948d3
Drishyaraju <drishyaraju13@gmail.com>
2024-05-19 23:00:18 +0530

    commit

---
+++ qtify/src/components/Feedback/Feedback.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Feedback.css";
+const Feedback = () => {
+    return(
+        <div>
+        <button>Give Feedback</button>
+        </div>
+    )
+}
+
+export default Feedback;
---
+++ qtify/src/components/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+import icon from "../../assets/QtifyIcon.png"
+import "./Logo.css";
+import React from "react";
+const Logo = () => {
+    return(
+        <div>
+        <img src={icon} alt="Logo" />
+        </div>
+    )
+}
+
+export default Logo;
+
